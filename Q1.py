cf86ef1fcd690b62dcf2117a824942ca623d68dd
Sunil kamat <sunil.kamat@tomtom.com>
2016-02-16 16:13:37 +0530

    program from codingbat

---
+++ Q1.py
@@ -0,0 +1,21 @@
+#Given an array of ints, return True if .. 1, 2, 3, .. appears in the array somewhere. ?
+
+def array123(nums):
+ n1 = set(nums)
+ if 1 and 2 and 3 in n1:
+  return True
+  
+ return False 
+
+
+""" here is another with 1,2,3 in sequence
+
+def array123(nums):
+  # Note: iterate with length-2, so can use i+1 and i+2 in the loop
+  for i in range(len(nums)-2):
+    if nums[i]==1 and nums[i+1]==2 and nums[i+2]==3:
+      return True
+  return False
+
+"""
+
